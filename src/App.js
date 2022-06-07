c7d778e08db45842be6032b508a80d81f19488ce
Kh-Filali <98891541+Kh-Filali@users.noreply.github.com>
2022-06-07 11:04:59 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <AddTodo />
+      <TodoList />
+    </header>
+  </div>
+  );
+}
+
+export default App;
